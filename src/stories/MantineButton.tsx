f945bd20326a5f352d5a5505bfcb04445abf6d60
Shungo-I <iririn199810710@gmail.com>
2025-06-15 08:25:49 +0900

    feat: create MantineButton component wrapper for Storybook

---
+++ src/stories/MantineButton.tsx
@@ -0,0 +1,12 @@
+import { Button } from '@mantine/core';
+import type { ButtonProps } from '@mantine/core';
+
+export interface StoryMantineButtonProps extends ButtonProps {
+  /** Button contents */
+  children: React.ReactNode;
+}
+
+/** Mantine Button component wrapper for Storybook */
+export const MantineButton = ({ children, ...props }: StoryMantineButtonProps) => {
+  return <Button {...props}>{children}</Button>;
+}; 
